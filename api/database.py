102e8f7107f3222fc34a4e6f388c23f9d2d5ba9b
Garth Mortensen <mortensengarth@hotmail.com>
2025-04-24 20:34:44 -0500

    Db: Establish db connection

--- api/database.py
+++ api/database.py
@@ -10,14 +10,15 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.exc import OperationalError
 import datetime
-
+from dotenv import load_dotenv
 
 # Database connection details from environment variables
-DB_USER = os.getenv("DB_USER", "myuser")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "password")
-DB_HOST = os.getenv("DB_HOST", "postgres")
+load_dotenv()
+DB_USER = os.getenv("DB_USER", "timeseriesuser")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "timeseriespass")
+DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = os.getenv("DB_PORT", "5432")
-DB_NAME = os.getenv("DB_NAME", "mydatabase")
+DB_NAME = os.getenv("DB_NAME", "timeseriesdb")
 
 # Construct database URL
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
