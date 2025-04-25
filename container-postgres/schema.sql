8a59124008f946730ead9226adc165c9f69d65db
Garth Mortensen <mortensengarth@hotmail.com>
2025-04-24 21:04:01 -0500

    Delete

--- container-postgres/schema.sql
+++
@@ -1,8 +0,0 @@
--- ddl schema
-create table cats (
-    id serial primary key,
-    name varchar(255) not null,
-    age integer,
-    loved boolean default true,
-    created_at timestamp default current_timestamp
-);
